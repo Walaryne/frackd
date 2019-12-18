3f32f75711f049da0f1e020f2649992104266fe3
Walaryne <hazardoustheradioactive@gmail.com>
2019-12-18 15:50:14 -0500

    Moved defines and inclusions to header file

--- src/frackd.c
+++ src/frackd.c
@@ -1,42 +1,4 @@
-#include <errno.h>
-#include <sys/epoll.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <stdio.h>
-#include <stdalign.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/inotify.h>
-#include <unistd.h>
-#include <syslog.h>
-
-#define MAX_EVENTS 16
-#define MAX_WATCH_DESCRIPTORS 512
-
-#ifdef NO_DAEMON
-
-#define DAEMON(x, y)
-
-#else
-
-#define DAEMON(x, y) daemon(x, y)
-#define USE_SYSLOG
-
-#endif
-
-#ifdef USE_SYSLOG
-
-#define WARN(...) syslog(LOG_DAEMON | LOG_INFO, __VA_ARGS__)
-
-#else
-
-#define WARN(...) dprintf(STDERR_FILENO, __VA_ARGS__)
-
-#endif
-
-#define WARN_LOG(...) WARN("FRACKD_LOG: " __VA_ARGS__)
-
-#define WARN_PE(x) WARN("FRACKD_ERROR: " x ": " "%s\n", strerror(errno))
+#include "frackd.h"
 
 int readfrackrc(char **paths, char **executables) {
 	int watchcount = 0;
